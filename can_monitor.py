b0ed2ad945d68ef1cd674e5766258a9c1533823e
Junior Asante <junior_a87@hotmail.com>
2021-09-19 12:44:01 +0200

    CAN messaging works!
    added simple can send and recieve

--- can_monitor.py
+++ can_monitor.py
@@ -32,5 +32,28 @@ from pprint import pprint
 # @click.option('-o', '--output', 'ofile_path',
 #               help='set generated file destination. Default ./')
 
-print("Hello world")
 
+def create_msg(frame_id,data_param):
+    return can.Message(arbitration_id=frame_id, data=data_param)
+
+print("Hello world")
+db = cantools.database.load_file('./J1939-DBC/J1939_demo.dbc')
+
+print("#### ")
+msg_2182 = db.get_message_by_name('EEC1')
+
+#print("printing msg 2192\n", msg_2182.signals)
+
+can_bus = can.interface.Bus('can0', bustype='socketcan')
+
+data_2182 = msg_2182.encode({'EngineSpeed':10.0})
+msg_send = create_msg(msg_2182.frame_id,data_2182)
+task_2182 = can_bus.send(msg_send)
+
+#message = can_bus.recv()
+#print("decoded msg:", db.decode_message(message.arbitration_id, message.data))
+
+#print("checkpoint0", task_2182)
+
+#message = can.Message(arbitration_id=example_message.frame_id, data=data)
+#can_bus.send(message)
