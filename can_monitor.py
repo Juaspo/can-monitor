1ee3eaf725c621eeefb53dc5ba3951b9f3639179
Junior Asante <junior_a87@hotmail.com>
2021-09-25 14:42:40 +0200

    Did some medium changes to GUI and main
    Updated can receive function
    updated can DBC file handling function
    added can decode function
    GUI displaying hardcoded values

--- can_monitor.py
+++ can_monitor.py
@@ -35,7 +35,6 @@ from tkinter import ttk
               help='set generated file destination. Default ./')
 
 
-
 def main(cfg_file: str, logging_level: str, dbc_file: str, ofile_path: str) -> int:
     logger = create_logger(logging_level)
     logger.info("Logging level set to: %s", logging_level)
@@ -50,15 +49,20 @@ def main(cfg_file: str, logging_level: str, dbc_file: str, ofile_path: str) -> i
 
     return 0
 
-def send_can():
+def send_can(arb_id: str, can_data: str):
+    '''
+    Send message on the canbus
+    input:
+        arb_id, data
+    return:
+        int
+    '''
     print("Hello world")
     db = cantools.database.load_file('./J1939-DBC/J1939_demo.dbc')
-
     print("#### ")
     msg_2182 = db.get_message_by_name('EEC1')
 
     #print("printing msg 2192\n", msg_2182.signals)
-
     can_bus = can.interface.Bus('can0', bustype='socketcan')
 
     data_2182 = msg_2182.encode({'EngineSpeed':10.0})
@@ -67,10 +71,29 @@ def send_can():
 
     return 0
 
+def get_db_object(logger: Logger, dbc_file_path: str):
+    logger.debug(f"Using DBC file: %s", dbc_file_path)
+    try:
+        db = cantools.database.load_file(dbc_file_path)
+        return db
+    except FileNotFoundError as e:
+        logger.error(f"DBC File not found: %s", e)
+    return None
+
+
+
+def decode_can_msg(logger: Logger, arb_id: str, can_data: str, db: object = None, dbc_file_path: str = None ) -> dict:
+    if dbc_file_path is not None:
+        db = get_db_object(logger, dbc_file_path)   
+    if db is None:
+        raise ValueError ('Expected either db object or dbc_file path parameter')
+        return None
+    return db.decode_message(arb_id, can_data)
+
+
 def receive_can():
     message = can_bus.recv()
     print("decoded msg:", db.decode_message(message.arbitration_id, message.data))
-
     print("checkpoint0", task_2182)
 
     # message = can.Message(arbitration_id=example_message.frame_id, data=data)
@@ -109,7 +132,8 @@ def create_logger(logging_level: str) -> Logger:
     '''
     logger = logging.getLogger(__name__)
     logger.setLevel(getattr(logging, logging_level.upper(), 10))
-    formatter = logging.Formatter('%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',  datefmt='%Y-%m-%d:%H:%M:%S')
+    formatter = logging.Formatter('%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                                  datefmt='%Y-%m-%d:%H:%M:%S')
     ch = logging.StreamHandler(sys.stdout)
     ch.setFormatter(formatter)
 
---
+++ can_monitor_gui.py
@@ -0,0 +1,189 @@
+'''
+can_monitor_gui.py
+GUI main for can-monitor
+
+
+'''
+from can_monitor import create_logger
+from can_monitor import get_config
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+
+top = Tk()
+top.minsize(width=250, height=150)
+
+top.title("Wake on lan")
+
+tabControl = ttk.Notebook(top)          # Create Tab Control
+tab1 = ttk.Frame(tabControl)            # Create a tab
+tabControl.add(tab1, text='WOL')      # Add the tab
+tabControl.pack(expand=1, fill="both")  # Pack to make visible
+
+#top.geometry("100x100")
+main_frame0 = Frame(tab1)
+main_frame0.pack()
+
+main_frame1 = Frame(main_frame0)
+main_frame1.pack()
+
+main_frame1_1 = Frame(main_frame0, highlightbackground="black", highlightthickness=1, pady=5)
+main_frame1_1.pack()
+
+label = Label(main_frame1, pady=20, width=24, relief=RIDGE, text="WOL", wraplength=250, bg="white", fg="black", font="sans 12 bold")
+label.pack()
+
+gen_label = Label(main_frame1_1, text="Broadcast address", fg="black", width = 17)
+gen_label.grid(row = 0, column = 0)
+general_ip_entry = Entry(main_frame1_1, width = 15)
+general_ip_entry.grid(row = 0, column = 1)
+
+main_frame1_2 = Frame(main_frame1_1)
+main_frame1_2.grid(row=1, column=0)
+
+repeat_label = Label(main_frame1_2, text="Repeat ping", fg="black", width = 10)
+repeat_label.grid(row = 0, column = 1)
+repeat_ping_entry = Entry(main_frame1_2, width = 3)
+repeat_ping_entry.grid(row = 0, column = 0)
+
+quit_after_wol_var=BooleanVar()
+
+quit_cb = Checkbutton(main_frame1_1, width = 12, text="Quit after", variable=quit_after_wol_var, onvalue=True, offvalue=False)
+quit_cb.grid(row = 1, column = 1)
+
+#test_button = Button(main_frame1_1, text="test", command=debug_def)
+#test_button.grid(row=2, column=0)
+
+frames = []
+mac_labels = []
+btn = []
+ip_labels = []
+
+############################################# Configurations
+
+tab3 = ttk.Frame(tabControl)            # Create a tab
+tabControl.add(tab3, text='Configure')      # Add the tab
+tabControl.pack(expand=1, fill="both")  # Pack to make visible
+
+conv_frame2 = Frame(tab3)
+conv_frame2.pack()
+
+
+################################## Functions
+
+def remove_buttons_and_labels():
+    global frames
+    global mac_labels
+    global btn
+    global ip_labels
+
+    for i in range(len(frames)):
+        frames[i].destroy()
+        mac_labels[i].destroy()
+        btn[i].destroy()
+        ip_labels[i].destroy()
+
+def quit_func():
+    print ("good bye")
+    top.destroy()
+
+
+def set_btn_labels():
+    '''
+    Functions for creating buttons and
+    text fields
+    '''
+
+    # config_content = get_config()
+    config_content = {"t0": 0, "t1": 100, "t2": 200}
+    number_of_items = len(config_content)
+    # if "Config" in config_content:
+    #     number_of_items -= 1
+    create_buttons_and_labels(number_of_items)
+
+    n = 0
+
+    for devices in config_content:
+        if devices != "Config":
+            try:
+                # print("number:",n)
+                btn[n].config(text = devices)
+                mac = 23
+                
+                ip_addr=config_content[devices]
+                ip_labels[n].config(text=ip_addr)
+
+                n += 1
+            except KeyError:
+                print("Error with key:", devices)
+    return
+
+def create_buttons_and_labels(nr_to_create):
+    global frames
+    global mac_labels
+    global btn
+    global ip_labels
+
+    for i in range(nr_to_create):
+        print("create:",i)
+        frames.append(Frame(main_frame0, pady=5))
+        frames[-1].pack()
+
+        mac_labels.append(Label(frames[-1], text="MAC", fg="black", font="Verdana 10 bold"))
+        mac_labels[-1].grid(row = 0, column = 0)
+
+        btn.append(Button(frames[-1], text="No host", width = 15, command=lambda n=i: btn_action(n)))
+        btn[-1].grid(row = 0, column = 1)
+
+        ip_labels.append(Label(frames[-1], text="IP", fg="black"))
+        ip_labels[-1].grid(row = 1, column = 0)
+
+def set_configuration():
+    # config_content = get_config()
+    config_content = "test"
+    if "test" in config_content:
+        try:
+            general_ip_entry.delete(0, END)
+            general_ip_entry.insert(0, "general ip entry")
+            repeat_ping_entry.delete(0, END)
+            repeat_ping_entry.insert(0, "99")
+
+        except Exception as e:
+            print("config error:", e)
+            label.config(text=f"CFG error: {e}")
+
+    else:
+        general_ip_entry.delete(0, END)
+        general_ip_entry.insert(0, "Bad config")
+        return False
+
+def main(argv):
+
+    set_btn_labels()
+    set_configuration()
+
+    # try:
+    #     opts, args = getopt.getopt(argv, "hs:t:", ["help"])
+    # except getopt.GetoptError:
+    #     print("Wrong input try -h for help")
+    #     sys.exit(2)
+
+    # for opt, arg in opts:
+    #     print("arg:", arg)
+    #     if opt in ("-h", "--help"):
+    #         print("\nTimeCounter help screen\n")
+    #         print("-h\tfor this help text\n-s\tfor auto start\n-t\tfor timestamp")
+    #         sys.exit()
+    #     elif opt == "-t":
+    #         timestamp(arg)
+    #         take_timestamp = True
+    #     elif opt == "-s":
+    #         run_end_clocking(arg)
+    #         autostart = True
+
+        # print ("auto start:", autostart, "timestamp:", take_timestamp, "argv:", argv, "opts:", opts)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
+top.mainloop()
